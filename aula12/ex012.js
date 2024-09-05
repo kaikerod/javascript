a208e694c2910816bfe9b396b7153c4aac2bb4cf
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2024-09-05 09:54:23 -0300

    .

---
+++ aula12/ex012.js
@@ -0,0 +1,10 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamnete ${hora} horas.`)
+if (hora < 12) {
+    console.log('Bom dia!')
+} else if (fora <=18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
