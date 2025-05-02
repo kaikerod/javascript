f1bcb511bc7d0782b69008b33d836dba9b5ca521
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-02 20:50:07 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,9 +1,10 @@
 alert('Bem vindo(a) ao jogo do numero secreto!');
 let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um numero entre 1 e 10');
 
 if (chute == numeroSecreto) {
-    console.log('Isso ai, voce descobriu o numero secreto (5)!');
+    alert(`Isso ai, voce descobriu o numero secreto! ${numeroSecreto}`);
 } else {
     alert('Voce errou :(')
 }
