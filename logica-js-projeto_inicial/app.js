089b96df7f5f985b540386b0ad38d1e7034bb599
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-02 21:10:08 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -6,5 +6,9 @@ let chute = prompt('Escolha um numero entre 1 e 10');
 if (chute == numeroSecreto) {
     alert(`Isso ai, voce descobriu o numero secreto! ${numeroSecreto}`);
 } else {
-    alert('Voce errou :(')
+    if (chute > numeroSecreto) {
+        alert(`O numero secreto e menor que ${chute}`);
+    } else {
+        alert(`O numero secreto e maior que ${chute}`)
+    }
 }
