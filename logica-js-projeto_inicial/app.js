f45a052790e0ab2c43c197030442b0a4f1bb4a72
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-05 19:49:56 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -5,14 +5,14 @@ let chute
 
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
-}
 
-if (chute == numeroSecreto) {
-    alert(`Isso ai, você descobriu o número secreto! ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
+    if (chute == numeroSecreto) {
+        alert(`Isso ai, você descobriu o número secreto! ${numeroSecreto}`);
     } else {
-        alert(`O número secreto é maior que ${chute}`)
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
     }
 }
