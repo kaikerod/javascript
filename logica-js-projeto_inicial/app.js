a4129ce2e098f22dbfc3703354cd73d96e74077a
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-05 20:18:35 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -20,4 +20,8 @@ while (chute != numeroSecreto) {
     }
 }
 
-alert(`Isso ai, você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativas.`);
+if (tentativas > 1) {
+    alert(`Isso ai, você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativas.`);
+} else {
+    alert(`Isso ai, você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativa.`);
+}
