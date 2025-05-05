42a8f0c19dfa79e3d566bb93aea639251d6e266a
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-05 19:59:10 -0300

    .

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,7 @@
 alert('Bem vindo(a) ao jogo do número secreto!');
 let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute
+console.log(numeroSecreto);
+let chute;
 
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
@@ -12,7 +12,7 @@ while (chute != numeroSecreto) {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}`)
+            alert(`O número secreto é maior que ${chute}`);
         }
     }
 }
