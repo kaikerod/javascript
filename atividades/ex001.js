da992a723277886e3bb829a51514ae112f065be0
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-08 20:47:31 -0300

    Update ex001.js

---
+++ atividades/ex001.js
@@ -0,0 +1,10 @@
+let sabado
+let domingo
+let diaNormal
+
+let semana = prompt('Qual é o dia da semana? ');
+    if(semana = sabado || domingo) {
+        print('Bom fim de semana! ')
+    } else {
+        print('Boa semana!')
+    }
