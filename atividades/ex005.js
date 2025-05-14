5c53f01e654deda8524de3fecb7324fb6878a11f
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-14 20:17:24 -0300

    Update ex005.js

--- atividades/ex005.js
+++ atividades/ex005.js
@@ -4,13 +4,14 @@ while (contador <= 10) {
     contador++;
 }
 
-// console.log('Boas Vindas');
+console.log('Boas Vindas');
 
-// let nome = prompt('Insira seu nome ');
-// console.log(`Olá ${nome}! `);
+let nome = prompt('Insira seu nome ');
+console.log(`Olá ${nome}! `);
 
 let valor1 = 5
 let valor2 = 10
 let resultado = (valor1 - valor2);
 
 console.log(`A diferença entre ${valor1} e ${valor2} é de ${resultado}`);
+
