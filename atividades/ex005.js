c1b6362287bd13b375b189c96128a2beed957a8f
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-14 20:19:37 -0300

    Update ex005.js

--- atividades/ex005.js
+++ atividades/ex005.js
@@ -15,3 +15,10 @@ let resultado = (valor1 - valor2);
 
 console.log(`A diferença entre ${valor1} e ${valor2} é de ${resultado}`);
 
+let idade = prompt('Qual é a sua idade? ');
+
+if (idade >= 18) {
+    console.log('Você é maior de idade.');
+} else {
+    console.log('Você é menor de idade');
+}
