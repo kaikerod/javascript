3c8acb783cc9fe55736cabca7a436b93b1f5c78b
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-21 20:22:03 -0300

    Update app.js

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Jogo do número secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+
+function verificarChute() {
+    console.log('O botão foi clicado!')
+}
