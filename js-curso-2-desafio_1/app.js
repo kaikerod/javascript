aa8f3f0dfb076052153d61b7182b0c878617b690
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-21 20:53:21 -0300

    .

--- js-curso-2-desafio_1/app.js
+++ js-curso-2-desafio_1/app.js
@@ -10,6 +10,13 @@ function exibirAlerta() {
 }
 
 function criarPergunta() {
-    nomeCidade = prompt('Digite o nome de uma cidade do Brasil');
-    alert(`Estive em ${nomeCidade} e lembrei de você!`)
+    let nomeCidade = prompt('Digite o nome de uma cidade do Brasil');
+    alert(`Estive em ${nomeCidade} e lembrei de você!`);
+}
+
+function somarNumeros() {
+    let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
+    let segundoNumero = parseInt(prompt('Digite o segundo número'));
+    let resultado = primeiroNumero + segundoNumero;
+    alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
 }
