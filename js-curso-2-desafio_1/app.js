d3e35295b7ea08f518bda886ec77d35e214b7035
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-21 20:45:35 -0300

    criei algumas functions

--- js-curso-2-desafio_1/app.js
+++ js-curso-2-desafio_1/app.js
@@ -4,3 +4,12 @@ titulo.innerHTML = 'Hora do desafio!';
 function verificarClique() {
     console.log('O botão console foi clicado');
 }
+
+function exibirAlerta() {
+    alert('Eu amo JS!');
+}
+
+function criarPergunta() {
+    nomeCidade = prompt('Digite o nome de uma cidade do Brasil');
+    alert(`Estive em ${nomeCidade} e lembrei de você!`)
+}
