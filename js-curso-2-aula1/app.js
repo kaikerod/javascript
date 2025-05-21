d7166997e0d8ebc4148ccce5ba126fb1f44ab2e9
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-21 20:22:53 -0300

    Update app.js

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -5,5 +5,5 @@ let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número de 1 a 10';
 
 function verificarChute() {
-    console.log('O botão foi clicado!')
+    console.log('O botão de chute foi clicado!')
 }
