af92a1bacb9605d7f7e08ec0dd615bffc0a1e227
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-05-22 19:23:17 -0300

    Update app.js

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,8 +1,13 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
 function verificarChute() {
     console.log('O botão de chute foi clicado!')
