ba42ec29cf98d257fdeb6537335551b8d465c663
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-06-02 20:08:30 -0300

    Update app2.js

--- js-curso-2-desafio_1/app2.js
+++ js-curso-2-desafio_1/app2.js
@@ -2,7 +2,17 @@ function exibirNoConsole() {
    console.log("Olá, mundo!")
 }
 
+exibirNoConsole();
+
 function inputNome() {
     let nome = 'Kaike'
     return console.log(`Olá, ${nome}!`);
+}
+
+inputNome();
+
+let numero = parseInt(prompt('Digite um número para eu calcular o dobro! '));
+
+function calcularDobro(numero) {
+    return numero * 2;
 }
