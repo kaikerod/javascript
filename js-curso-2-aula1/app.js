9c56bab0c29c6b6724da03b0099b500362dae8eb
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-06-03 19:59:59 -0300

    Update app.js

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -26,8 +26,8 @@ function verificarChute() {
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} tentativas!`;
-        exibirTextoNaTela('p', 'Você descobriu o número secreto com 5 tentativas!');
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa} tentativas!`;
+        exibirTextoNaTela('p', mensagemTentativas);
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
