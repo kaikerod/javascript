44b029e7f8b7a0023143b8eacb2f3a80f9e7de29
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-06-05 20:07:25 -0300

    Update app.js

--- desafios/desafio-1/app.js
+++ desafios/desafio-1/app.js
@@ -1,7 +1,10 @@
-let peso = parseFloat(prompt('Qual o é o seu peso corporal em KG? '));
-let altura = parseFloat(prompt('Qual a sua altura em metros?'));
-
 function calcularImc() {
-    peso / altura
-    alert(`Seu IMC é de ${calcularImc}`);
+    let peso = parseFloat(prompt('Qual o é o seu peso corporal em KG? '));
+    let altura = parseFloat(prompt('Qual a sua altura em metros?'));
+    return
 }
+
+let imc = peso / (altura * altura);
+alert(`Seu IMC é de ${imc}!`);
+
+calcularImc();
