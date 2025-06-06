f7e2bf7d2d77cafd8b28f9cf463205676829bb61
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-06-06 20:09:48 -0300

    Update dolar.js

--- desafios/desafio-1/dolar.js
+++ desafios/desafio-1/dolar.js
@@ -3,3 +3,8 @@ function converterDolarParaReal(valorEmDolar) {
   let valorEmReais = valorEmDolar * cotacaoDolar;
   return valorEmReais.toFixed(2);
 }
+
+// Exemplo de uso
+let valorEmDolar = 50;
+let valorEmReais = converterDolarParaReal(valorEmDolar);
+console.log(`${valorEmDolar} dólares equivalem a R$ ${valorEmReais}`);
