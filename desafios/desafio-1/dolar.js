058335dc7dc38f1760d18ffdf8eb17075f8a8cf8
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-06-07 12:55:16 -0300

    Update dolar.js

--- desafios/desafio-1/dolar.js
+++ desafios/desafio-1/dolar.js
@@ -2,7 +2,7 @@ function converteDolarParaReal(BRL, USD) {
     let real = parseFloat(prompt('Insira um valor em reais para ser convertido para dólar'));
     let dolar = 4.80;
     let valorConvertido = real * dolar;
-    return cambio;
+    return valorConvertido;
 }
 
 console.log(converteDolarParaReal());
