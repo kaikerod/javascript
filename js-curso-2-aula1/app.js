2936467abb33ee0d926c44871a971f4ae08d6b97
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-06-09 20:37:29 -0300

    Update app.js

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,8 +1,3 @@
-// let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Jogo do número secreto';
-
-// let paragrafo = document.querySelector('p');
-// paragrafo.innerHTML = 'Escolha um número de 1 a 10';
 
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
