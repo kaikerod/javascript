e837c07da0f72f4f0a44723f9d2eefeac0f201de
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-06-13 19:03:25 -0300

    Update contagem.js

---
+++ desafios/desafio-3/contagem.js
@@ -0,0 +1,13 @@
+let arrayNumeros = [1, 2, 3, 4, 5];
+let arrayStrings = ["a", "b"];
+
+function contagem(arrayNumeros) {
+    console.log(arrayNumeros.length);
+}
+
+function contagem(arrayStrings) {
+    console.log(arrayStrings.length);
+}
+
+contagem(arrayNumeros);
+contagem(arrayStrings);
