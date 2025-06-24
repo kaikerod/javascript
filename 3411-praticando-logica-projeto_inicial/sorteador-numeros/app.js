5f09aacef417ccb7cf0cda5d284e6d118a0b2ca1
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-06-24 20:25:54 -0300

    adicionando functions

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,5 +1,14 @@
 function sortearNumero() {
-    let quantidadeDeNumeros = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let quantidadeDeNumeros = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+
+    for (let i = 0; i < quantidadeDeNumeros; i++) {
+}
+}
+
+function sortearNumeros(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
