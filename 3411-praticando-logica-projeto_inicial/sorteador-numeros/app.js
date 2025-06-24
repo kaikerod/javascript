bc5eeaa320b3f61a3369828b1339871637039db8
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-06-23 21:20:35 -0300

    criando uma function

---
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,5 @@
+function sortearNumero() {
+    let quantidadeDeNumeros = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+}
