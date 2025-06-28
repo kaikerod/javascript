5cf54f0cc44dfb39ef739a8e6d8b9cc0433ad9d8
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-06-28 16:49:09 -0300

    adicionei a função de reiniciar

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -21,8 +21,29 @@ function sortearNumero() {
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
 
+    alterarStatusBotaoReiniciar();
 }
 
 function sortearNumeros(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotaoReiniciar() {
+    let botaoReiniciar = document.getElementById('btn-reiniciar');
+
+    if (botaoReiniciar.classList.contains('container__botao-desabilitado')) {
+        botaoReiniciar.classList.remove('container__botao-desabilitado');
+        botaoReiniciar.classList.add('container__botao');
+    } else {
+        botaoReiniciar.classList.remove('container__botao');
+        botaoReiniciar.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotaoReiniciar();
 }
