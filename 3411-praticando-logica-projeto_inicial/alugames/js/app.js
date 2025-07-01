13c42fcde397750ea299a99c9aa4b306554b2f36
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2025-07-01 20:28:42 -0300

    Tentando resolver o desafio

---
+++ 3411-praticando-logica-projeto_inicial/alugames/js/app.js
@@ -0,0 +1,6 @@
+function alterarStatus(monopoly) {
+    let botaoAlugarMonopoly = document.getElementsByClassName('dashboard__item__button');
+
+    if (botaoAlugarMonopoly.classList.contains('dashboard__item__button')) {
+        
+}
